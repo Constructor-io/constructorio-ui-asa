ccb36bf391d9f3c8817c8dedfc4eea90a3641294
Ahmad Mudaafi <ahmad.mudaafi@constructor.io>
2024-07-12 00:56:17 +0800

    add tests

---
+++ src/hooks/useRequestConfigs.ts
@@ -0,0 +1,38 @@
+import { useCioAsaContext } from './useCioAsaContext';
+import { RequestConfigs } from '../types';
+
+interface UseRequestConfigsReturn {
+  requestConfigs: RequestConfigs;
+  setRequestConfigs: (configsToUpdate: Partial<RequestConfigs>) => void;
+}
+
+export default function useRequestConfigs(): UseRequestConfigsReturn {
+  const context = useCioAsaContext();
+  if (!context) {
+    throw new Error('This Hook needs to be called within the C.io PLP Context Provider.');
+  }
+
+  const { urlHelpers, staticRequestConfigs } = context;
+  const { getUrl, setUrl, getStateFromUrl, getUrlFromState } = urlHelpers;
+
+  const url = getUrl();
+  const urlRequestConfigs = url ? getStateFromUrl(url) : { domain: 'assistant' };
+
+  const requestConfigs: RequestConfigs = { ...staticRequestConfigs, ...urlRequestConfigs };
+
+  const setRequestConfigs = (configsToUpdate: Partial<RequestConfigs>) => {
+    const oldUrl = getUrl();
+    if (!oldUrl) {
+      throw new Error('getUrl returns undefined when attempting to call setRequestConfigs');
+    }
+
+    const urlObj = new URL(oldUrl);
+    const oldRequestConfigs = oldUrl ? getStateFromUrl(oldUrl) : { domain: 'assistant' };
+    const newRequestConfigs = { ...oldRequestConfigs, ...configsToUpdate };
+    const newUrl = getUrlFromState(newRequestConfigs, urlObj);
+
+    setUrl(newUrl);
+  };
+
+  return { requestConfigs, setRequestConfigs };
+}
