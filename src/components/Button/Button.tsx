0665312ee5b1bb385ae844bb24def4952a2e8a21
Vincent Galloro <vincentpetergalloro@gmail.com>
2024-08-14 14:27:22 -0400

    create product card

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 interface ButtonProps {
   text?: string;
+  fullWidth?: boolean;
 }
 
-export default function Button({ text }: ButtonProps) {
+export default function Button({ text, fullWidth }: ButtonProps) {
   return (
-    <button type='button' className='cio-asa-button'>
+    <button
+      type='button'
+      className='cio-asa-button'
+      style={fullWidth ? { width: '100%' } : undefined}>
       {text || 'Click Me'}
     </button>
   );
