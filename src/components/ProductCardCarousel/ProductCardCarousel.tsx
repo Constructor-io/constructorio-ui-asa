47fa11fcc26fab6f1ab717d5ada7def60e94dc57
Vincent Galloro <vincentpetergalloro@gmail.com>
2024-08-26 11:42:49 -0400

    css and package fixes

--- src/components/ProductCardCarousel/ProductCardCarousel.tsx
+++ src/components/ProductCardCarousel/ProductCardCarousel.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Slider, { Settings } from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import NavArrow from '../NavArrow/NavArrow';
 
-export default function ProductCardCarousel(props: React.PropsWithChildren) {
-  const { children } = props;
-  const settings: Settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    prevArrow: <NavArrow direction='Prev' />,
-    nextArrow: <NavArrow direction='Next' />,
-  };
+const defaultSettings: Settings = {
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: <NavArrow direction='Prev' />,
+  nextArrow: <NavArrow direction='Next' />,
+};
+
+interface ProductCardCarouselProps {
+  productDisplayCount?: number;
+  productCountPerScroll?: number;
+}
+
+export default function ProductCardCarousel(
+  props: React.PropsWithChildren & ProductCardCarouselProps,
+) {
+  const { children, productDisplayCount, productCountPerScroll } = props;
+
+  const settings = useMemo(
+    () => ({
+      ...defaultSettings,
+      slidesToShow: productDisplayCount ?? defaultSettings.slidesToShow,
+      slidesToScroll: productCountPerScroll ?? defaultSettings.slidesToScroll,
+    }),
+    [productCountPerScroll, productDisplayCount],
+  );
 
   return <Slider {...settings}>{children}</Slider>;
 }
