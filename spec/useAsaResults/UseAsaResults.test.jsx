1148b173a04de57fd3c3e91a207b945cde2fec1b
Vincent Galloro <vincentpetergalloro@gmail.com>
2024-10-15 23:38:47 -0400

    create test [WORK IN PROGRESS]

---
+++ spec/useAsaResults/UseAsaResults.test.jsx
@@ -0,0 +1,69 @@
+import { renderHook, waitFor } from '@testing-library/react';
+import useAsaResults from '../../src/hooks/useAsaResults';
+
+describe('Testing Hook: useBrowseResults', () => {
+  afterEach(() => {
+    jest.restoreAllMocks(); // This will reset all mocks after each test
+    jest.clearAllMocks();
+  });
+
+  test('Should return a PlpBrowseResponse Object', async () => {
+    const { result } = renderHookServerSideWithCioAsa(() => useAsaResults());
+
+    await waitFor(() => {
+      const response = result?.current.browseResults;
+
+      expect(response?.resultId).not.toBeUndefined();
+      expect(response?.totalNumResults).not.toBeUndefined();
+      expect(response?.refinedContent).not.toBeUndefined();
+      expect(response?.groups).not.toBeUndefined();
+      expect(response?.results?.length).not.toBeUndefined();
+      expect(response?.facets?.length).not.toBeUndefined();
+      expect(response?.sortOptions?.length).not.toBeUndefined();
+      expect(response?.rawResponse).not.toBeUndefined();
+    });
+  });
+
+  it('Should not fetch results on first render with initialBrowseResponse', async () => {
+    const initialBrowseResponse = transformBrowseResponse(mockBrowseResponse);
+
+    renderHookWithCioPlp(() => useBrowseResults({ initialBrowseResponse }));
+
+    await waitFor(() => {
+      expect(mockConstructorIOClient.browse.getBrowseResults).not.toHaveBeenCalled();
+    });
+  });
+
+  test('Should receive parameters from useRequestConfigs correctly', async () => {
+    const filters = { Color: ['Phantom Ink'] };
+    const page = 2;
+    const resultsPerPage = 100;
+
+    const url = getUrlFromState(
+      { filterValue: '123', filters, resultsPerPage, page },
+      { baseUrl: 'https://example.com/browse/' },
+    );
+    mockLocation.href = url;
+
+    renderHookWithCioPlp(() => useBrowseResults());
+
+    await waitFor(() => {
+      expect(mockConstructorIOClient.browse.getBrowseResults).toHaveBeenCalledWith(
+        'group_id',
+        '123',
+        {
+          page,
+          filters,
+          resultsPerPage,
+        },
+      );
+    });
+  });
+
+  test('Should throw error if used outside Context Provider', () => {
+    const spy = jest.spyOn(console, 'error');
+    spy.mockImplementation(() => {});
+    expect(() => renderHook(() => useBrowseResults())).toThrow();
+    spy.mockRestore();
+  });
+});
