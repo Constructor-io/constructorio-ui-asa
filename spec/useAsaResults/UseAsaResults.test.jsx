7f3a10be9174828aeaf3179b5810f9b1f8d4bd76
Vincent Galloro <vincentpetergalloro@gmail.com>
2024-10-17 10:30:47 -0400

    add tests

--- spec/useAsaResults/UseAsaResults.test.jsx
+++ spec/useAsaResults/UseAsaResults.test.jsx
@@ -1,69 +1,72 @@
-import { renderHook, waitFor } from '@testing-library/react';
+import { renderHook } from '@testing-library/react';
+import { delay, renderHookWithCioAsa } from '../test-utils';
 import useAsaResults from '../../src/hooks/useAsaResults';
 
-describe('Testing Hook: useBrowseResults', () => {
-  afterEach(() => {
-    jest.restoreAllMocks(); // This will reset all mocks after each test
-    jest.clearAllMocks();
+// example data that could be returned by the ASA Client
+const mockGroups = [
+  { done: false, value: { type: 'group', group: 'a' } },
+  { done: false, value: { type: 'search_result', result: 'b' } },
+  { done: false, value: { type: 'search_result', result: 'c' } },
+  { done: false, value: { type: 'group', group: 'd' } },
+  { done: false, value: { type: 'search_result', result: 'e' } },
+  { done: true },
+];
+
+// data we expect the hook to return
+const expectedGroups = [
+  {
+    group: { group: 'a', type: 'group' },
+    searchResults: [
+      { result: 'b', type: 'search_result' },
+      { result: 'c', type: 'search_result' },
+    ],
+  },
+  { group: { group: 'd', type: 'group' }, searchResults: [{ result: 'e', type: 'search_result' }] },
+];
+
+// mock client made to feed the mockGroups in the same way as the real client
+const mockClient = {
+  assistant: {
+    getAssistantResultsStream: () => ({
+      getReader: () => {
+        let readIndex = -1;
+
+        return {
+          read: () => {
+            readIndex += 1;
+            return mockGroups[readIndex];
+          },
+          cancel: () => {},
+        };
+      },
+    }),
+  },
+};
+
+describe('Testing Hook: useAsaResults', () => {
+  it('Should stream results correctly', async () => {
+    const res = renderHookWithCioAsa(() => useAsaResults('picnic'), {
+      initialProps: { cioClient: mockClient },
+    });
+
+    res.rerender();
+
+    // I tried to find a better way to wait for the state update. Nothing worked
+    await delay(500);
+
+    const { groups } = res.result.current;
+
+    expect(groups).toStrictEqual(expectedGroups);
   });
 
-  test('Should return a PlpBrowseResponse Object', async () => {
-    const { result } = renderHookServerSideWithCioAsa(() => useAsaResults());
-
-    await waitFor(() => {
-      const response = result?.current.browseResults;
-
-      expect(response?.resultId).not.toBeUndefined();
-      expect(response?.totalNumResults).not.toBeUndefined();
-      expect(response?.refinedContent).not.toBeUndefined();
-      expect(response?.groups).not.toBeUndefined();
-      expect(response?.results?.length).not.toBeUndefined();
-      expect(response?.facets?.length).not.toBeUndefined();
-      expect(response?.sortOptions?.length).not.toBeUndefined();
-      expect(response?.rawResponse).not.toBeUndefined();
-    });
-  });
-
-  it('Should not fetch results on first render with initialBrowseResponse', async () => {
-    const initialBrowseResponse = transformBrowseResponse(mockBrowseResponse);
-
-    renderHookWithCioPlp(() => useBrowseResults({ initialBrowseResponse }));
-
-    await waitFor(() => {
-      expect(mockConstructorIOClient.browse.getBrowseResults).not.toHaveBeenCalled();
-    });
-  });
-
-  test('Should receive parameters from useRequestConfigs correctly', async () => {
-    const filters = { Color: ['Phantom Ink'] };
-    const page = 2;
-    const resultsPerPage = 100;
-
-    const url = getUrlFromState(
-      { filterValue: '123', filters, resultsPerPage, page },
-      { baseUrl: 'https://example.com/browse/' },
-    );
-    mockLocation.href = url;
-
-    renderHookWithCioPlp(() => useBrowseResults());
-
-    await waitFor(() => {
-      expect(mockConstructorIOClient.browse.getBrowseResults).toHaveBeenCalledWith(
-        'group_id',
-        '123',
-        {
-          page,
-          filters,
-          resultsPerPage,
-        },
-      );
-    });
+  it('Should throw error if no intent is passed', () => {
+    expect(() => renderHookWithCioAsa(() => useAsaResults(''))).toThrow();
   });
 
   test('Should throw error if used outside Context Provider', () => {
     const spy = jest.spyOn(console, 'error');
     spy.mockImplementation(() => {});
-    expect(() => renderHook(() => useBrowseResults())).toThrow();
+    expect(() => renderHook(() => useAsaResults('picnic'))).toThrow();
     spy.mockRestore();
   });
 });
